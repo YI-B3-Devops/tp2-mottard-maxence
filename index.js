fb17f0723dd9bb26226920b71b3277cb29564660
MaxenceMottard <mottard.maxence@gmail.com>
2019-12-17 15:23:32 +0100

    Add redis into project

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
-const app = express();
+const app = express()
 const {Client} = require('pg')
+const redis = require("redis")
 
 const pgClient = new Client({
   user: process.env.DB_USER,
@@ -10,6 +11,7 @@ const pgClient = new Client({
   database: process.env.DB_NAME,
   port: 5432,
 })
+const redisClient = redis.createClient({ host: 'devops_tp_redis' });
 
 app.use(cors())
 app.use(express.json())
@@ -18,10 +20,25 @@ app.get('/', (req, res) => {
   res.json({ message: "Hello World" })
 })
 
-app.get('/status', (req, res) => {
-  // {
-  //   "status": ​"OK"​, "postgresUptime": ​String​, "redisConnectedClients": ​Number
-  // }
+app.get('/status', async (req, res) => {
+  const postgresQuery = "SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;";
+  const result = await pgClient.query(postgresQuery)
+  const uptime = result.rows[0].uptime
+  const uptimeString = () => {
+    let time = ""
+
+    time += uptime.hours ? `${uptime.hours}h ` : ""
+    time += uptime.minutes ? `${uptime.minutes}m ` : ""
+    time += uptime.seconds ? `${uptime.seconds}s` : ""
+
+    return time
+  }
+
+  res.json({
+    status: 'ok',
+    postgresUptime: uptimeString(),
+    redisConnectedClients: Number(redisClient.server_info.connected_clients)
+  })
 })
 
 const run = async () => {
