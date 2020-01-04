86b90847a8900419f13fb2f6fb12e040a93e701f
MaxenceMottard <mottard.maxence@gmail.com>
2020-01-04 11:58:48 +0100

    Fix nginx configuration

--- index.js
+++ index.js
@@ -2,54 +2,52 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const {Client} = require('pg')
-const redis = require("redis")
+const redis = require('redis')
 
 const pgClient = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
 })
-const redisClient = redis.createClient({ host: 'devops_tp_redis' });
+const redisClient = redis.createClient({host: 'devops_tp_redis'})
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello World" })
+    res.json({message: 'Hello World'})
 })
 
 app.get('/status', async (req, res) => {
-  const postgresQuery = "SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;";
-  const result = await pgClient.query(postgresQuery)
-  const uptime = result.rows[0].uptime
-  const uptimeString = () => {
-    let time = ""
+    const postgresQuery = 'SELECT date_trunc(\'second\', current_timestamp - pg_postmaster_start_time()) as uptime;'
+    const result = await pgClient.query(postgresQuery)
+    const uptime = result.rows[0].uptime
+    const uptimeString = () => {
+        let time = ''
 
-    time += uptime.hours ? `${uptime.hours}h ` : ""
-    time += uptime.minutes ? `${uptime.minutes}m ` : ""
-    time += uptime.seconds ? `${uptime.seconds}s` : ""
+        time += uptime.hours ? `${uptime.hours}h ` : ''
+        time += uptime.minutes ? `${uptime.minutes}m ` : ''
+        time += uptime.seconds ? `${uptime.seconds}s` : ''
 
-    return time
-  }
+        return time
+    }
 
-  res.json({
-    status: 'ok',
-    postgresUptime: uptimeString(),
-    redisConnectedClients: Number(redisClient.server_info.connected_clients)
-  })
+    res.json({
+        status: 'ok',
+        postgresUptime: uptimeString(),
+        redisConnectedClients: Number(redisClient.server_info.connected_clients)
+    })
 })
 
-const run = async () => {
-  try {
-    await pgClient.connect()
-    const port = process.env.PORT
-    app.listen(port, () => console.log(`Server started. PORT : ${port}`))
-  } catch (e) {
-    console.log(e)
-    console.error("Unable to connect to the postgreSQL database")
-  }
-}
+const { PORT } = process.env
+app.listen(PORT, () => {
+    pgClient.connect()
+    console.log(`Server started. PORT : ${PORT}`)
+})
 
-run()
+
+// location = /en {
+// return 302 /en/;
+// }
